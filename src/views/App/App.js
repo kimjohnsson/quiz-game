ce554525d98813c2cae4ed997a70e7266a02c3a5
Kim <kim.johnsson@telia.com>
2019-12-10 22:34:46 +0100

    quis component getting 10 questions

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -8,6 +8,7 @@ const App = () => {
   const [data, setData] = useState()
   const [game, setGame] = useState({ newGame: false })
 
+  // Fetch quiz data
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch('https://opentdb.com/api.php?amount=50&category=28&type=multiple')
@@ -18,13 +19,28 @@ const App = () => {
   }, []);
 
   const newGame = () => {
-    setGame({ newGame: true })
+
+    // create a array of 10 numbers which represents the index of the selected questions
+    let selectedQuestions = []
+
+    let num = Math.floor(Math.random() * 49) + 1;
+    selectedQuestions.push(num)
+
+    // check so that the same question is not selected multiple times
+    while (selectedQuestions.length < 10) {
+      let num = Math.floor(Math.random() * 49) + 1;
+      if (!selectedQuestions.includes(num)) {
+        selectedQuestions.push(num)
+      }
+    }
+
+    setGame({ newGame: true, selectedQuestions: selectedQuestions })
   }
 
   return (
     <React.Fragment>
       {game.newGame ?
-        <Quiz data={data} /> :
+        <Quiz data={data} selectedQuestions={game.selectedQuestions} /> :
         <main>
           <h1>Quiz Game</h1>
           <button onClick={newGame}>New Game</button>
--- src/views/Quiz/Quiz.js
+++ src/views/Quiz/Quiz.js
@@ -1,13 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
-const Quiz = (data) => {
+const Quiz = (props) => {
 
-  const [question, setQuestion] = useState()
+  const [questions, setQuestions] = useState([])
+  const [index, setIndex] = useState(0)
 
-  console.log(data)
+  useEffect(() => {
+    const selectQuestions = () => {
+      let selectedQuestions = []
+      props.selectedQuestions.forEach(questionIndex => {
+        selectedQuestions.push(props.data[questionIndex])
+      });
+      setQuestions(selectedQuestions)
+    };
+    selectQuestions();
+  }, [props.data, props.selectedQuestions]);
+
+  const renderQuestions = () => {
+    if (questions.length) {
+      return (
+        <div>
+          <h1>{questions[index].question.replace(/(&quot;)/g, "\"").replace(/(&#039;)/g, "'")}</h1>
+        </div>
+      )
+    }
+  }
+
+  const nextQuestion = () => {
+    setIndex(index + 1)
+    console.log(index)
+  }
+
   return (
     <main>
-      <h1>Quiz</h1>
+      {renderQuestions()}
+      <button onClick={nextQuestion}>Next</button>
     </main>
   )
 }
