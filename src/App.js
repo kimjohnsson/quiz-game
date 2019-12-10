02b63a5e8b8f1455551d72f3825ad133d3a6f798
Kim <kim.johnsson@telia.com>
2019-12-10 16:34:20 +0100

    project set up

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+
+function App() {
+  return (
+    <Container>
+      <h1>Quiz Game</h1>
+    </Container>
+  );
+}
+
+export default App;
