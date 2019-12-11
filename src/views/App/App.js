e4b56fe961a1277fd48502c814e17a4a98104c41
Kim <kim.johnsson@telia.com>
2019-12-11 17:00:25 +0100

    moved code from App to Quiz component

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -6,7 +6,8 @@ import './StyledApp.scss'
 const App = () => {
 
   const [data, setData] = useState()
-  const [game, setGame] = useState({ newGame: false })
+  const [game, setGame] = useState({ play: false })
+
 
   // Fetch quiz data
   useEffect(() => {
@@ -18,36 +19,23 @@ const App = () => {
     fetchData();
   }, []);
 
-  const newGame = () => {
 
-    // create a array of 10 numbers which represents the index of the selected questions
-    let selectedQuestions = []
-
-    let num = Math.floor(Math.random() * 49) + 1;
-    selectedQuestions.push(num)
-
-    // check so that the same question is not selected multiple times
-    while (selectedQuestions.length < 10) {
-      let num = Math.floor(Math.random() * 49) + 1;
-      if (!selectedQuestions.includes(num)) {
-        selectedQuestions.push(num)
-      }
-    }
-
-    setGame({ newGame: true, selectedQuestions: selectedQuestions })
+  const playGame = () => {
+    setGame({ play: true })
   }
+
 
   return (
     <React.Fragment>
-      {game.newGame ?
-        <Quiz data={data} selectedQuestions={game.selectedQuestions} /> :
+      {game.play ?
+        <Quiz data={data} /> :
         <main>
           <h1>Quiz Game</h1>
           <p>This is a 10 question quiz about vehicles, you will have 15 seconds on every question to select an answer.</p>
           <p>you will have 2 lifelines one "50/50" where two of the wrong answers will disappear, and one "+ 10sec" where you will get 10 seconds more to answer the current question.</p>
           <p>After the quiz is finished your score will be displayed on the screen.</p>
           <p>Good Luck!</p >
-          <button onClick={newGame}>Play</button>
+          <button onClick={playGame}>Play</button>
         </main >
       }
       <Footer />
--- src/views/Quiz/Quiz.js
+++ src/views/Quiz/Quiz.js
@@ -5,34 +5,62 @@ import './StyledQuiz.scss'
 const Quiz = (props) => {
 
   const [questions, setQuestions] = useState([])
+  const [questionNumbers, setQuestionNumbers] = useState([])
   const [index, setIndex] = useState(0)
   const [score, setScore] = useState(0)
 
-  // set 10 questions to state
+
+  useEffect(() => {
+    const selectRandomQuestions = () => {
+      // create a array of 10 numbers which represents the index of the selected questions
+      let randomQuestionNumbers = []
+
+      let num = Math.floor(Math.random() * 49) + 1;
+      randomQuestionNumbers.push(num)
+
+      // check so that the same question is not selected multiple times
+      while (randomQuestionNumbers.length < 10) {
+        let num = Math.floor(Math.random() * 49) + 1;
+        if (!randomQuestionNumbers.includes(num)) {
+          randomQuestionNumbers.push(num)
+        }
+      }
+      setQuestionNumbers(randomQuestionNumbers)
+    }
+    selectRandomQuestions()
+  }, [])
+
+
+  // save 10 questions in state
   useEffect(() => {
     const selectQuestions = () => {
       let selectedQuestions = []
-      props.selectedQuestions.forEach(questionIndex => {
+      questionNumbers.forEach(questionIndex => {
         selectedQuestions.push(props.data[questionIndex])
       });
       setQuestions(selectedQuestions)
     };
-    selectQuestions();
-  }, [props.data, props.selectedQuestions]);
+
+    if (questionNumbers.length) {
+      selectQuestions();
+    }
+  }, [props.data, questionNumbers]);
+
 
   // render question
   const renderQuestions = () => {
-    if (questions.length) {
+    if (questions.length && index < questions.length) {
       return <h1>{questions[index].question.replace(/(&quot;)/g, "\"").replace(/(&#039;)/g, "'")}</h1>
     }
   }
 
+
   // render answers
   const renderAnswers = () => {
-    if (questions.length) {
+    if (questions.length && index < questions.length) {
 
       // place the correct answer in a random place together with the incorrect answers
-      let num = Math.floor(Math.random() * 3) + 1;
+      let num = Math.floor(Math.random() * 4);
       let answerAlternatives = [...questions[index].incorrect_answers]
       answerAlternatives.splice(num, 0, questions[index].correct_answer)
       return (
@@ -43,21 +71,24 @@ const Quiz = (props) => {
     }
   }
 
+
   // display next question and update score
   const nextQuestion = (e) => {
-    if (questions[index].correct_answer === e.target.innerText) {
+    if (questions[index].correct_answer.replace(/(&amp;)/g, " & ") === e.target.innerText) {
       setScore(score + 1)
     }
     setIndex(index + 1)
   }
 
+
   return (
-    <main>
-      {renderQuestions()}
-      <div className="answers">
-        {renderAnswers()}
-      </div>
-    </main>
+    index < 10 ?
+      <main>
+        {renderQuestions()}
+        < div className="answers">
+          {renderAnswers()}
+        </div>
+      </main > : <main><h1>score: {score}/10</h1></main>
   )
 }
 
